0f54a42ce92bc6dd4fec2cb6de8cc9501d1c9aa1
DanielPetk <danielpetkoski@hotmail.com>
2024-09-19 00:57:41 -0400

    Gsap and starting to test things

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useRef } from 'react'
+import gsap from 'gsap'
+import {useGSAP} from '@gsap/react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useGSAP(() => {
+    gsap.from('.penis', {opacity: 0})
+  });
+  
 
   return (
-    <>
-      <div>  
-      </div>
+    <>   
+      <br></br>
+      <br></br>
+      <div className="font-argon font-extrabold text-green-500 text-5xl drop-shadow-glow content-center text-center penis" >What is up fans</div>
     </>
   )
 }
 
 export default App
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "argon": ["Argon", "sans-serif"],
+      },
+      dropShadow: {
+        glow: [
+          "0 0px 25px rgba(34, 197, 94, .25)",
+          "0 0px 65px rgba(34, 197, 94, .7)"
+        ]
+      },
+      colors: {
+        primary: '#10141a',
+        secondary: '#0a0d11'
+      }
+    },
   },
   plugins: [],
 }
