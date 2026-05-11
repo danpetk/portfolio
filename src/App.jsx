2c0637e8968d8b60e9b00efdafa01bca7ce157bb
Daniel Petkoski <danielpetkoski@outlook.com>
2026-05-10 21:42:04 -0400

    header change

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div className="grid grid-cols-1 px-12 md:px-16 place-items-center overflow-visible min-w-[350px] mb-24">
-        <GlowingHeader headerTextArray={["./introduction", "welcome to my website"]}/>
+        <GlowingHeader headerTextArray={["hello there", "welcome to my website"]}/>
         <BriefInfo/>
         <Education/>
         <WorkExperience/>
